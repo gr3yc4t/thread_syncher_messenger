f0aa43922a568f732db15f8d2434d5dce592f203
gr3yc4t <gr3yc4t@protonmail.com>
2020-09-02 21:26:17 +0200

    Client tool: added concurrent tests
    Implemented concurrent tests on both read/write operation of a group

--- tool/main.c
+++ tool/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h> //For sleep()/nanosleep()
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -9,6 +10,11 @@
 #define IOCTL_INSTALL_GROUP _IOW('X', 99, group_t*)
 
 #define THREAD_NUM 64
+
+#define NO_MSG_PRESENT -10
+#define MSG_INVALID_FORMAT -11
+#define MSG_SIZE_ERROR  -12
+#define MEMORY_ERROR -13
 
 
 typedef struct t_message{
@@ -31,6 +37,21 @@ typedef struct group_t {
 pthread_t tid[THREAD_NUM];
 
 
+void t_nanosleep(long _nanoseconds){
+
+    struct timespec interval = {
+        .tv_sec = 0,
+        .tv_nsec = _nanoseconds
+    };
+
+    struct timespec interval2;
+
+    nanosleep(&interval, &interval2);
+
+}
+
+
+
 void concurrentRead(void *args){
 
     pthread_t id = pthread_self();
@@ -44,10 +65,35 @@ void concurrentRead(void *args){
         return -1;
     }
 
-    char buffer[256];
-    int ret = fread(buffer, sizeof(char), sizeof(char)*256, fd);
+    //Random Sleep 1
+    long sleep_time1 = rand()%10000;
+    usleep(sleep_time1);
 
 
+
+    msg_t msg;
+    int ret = fread(&msg, sizeof(msg_t), 1, fd);
+
+    if(!ret){
+        switch (ret)
+        {
+        case NO_MSG_PRESENT:
+            printf("[T-%ld/R] No msg. present\n", id);
+            break;
+        case MSG_SIZE_ERROR:
+            printf("[T-%ld/R] Message too large for current limits\n", id);
+            break;        
+        default:
+            printf("[T-%ld/R] Unknow error\n", id);
+            break;
+        }
+    }
+
+    //Random Sleep 2
+    long sleep_time2 = rand()%10000;
+    usleep(sleep_time2);
+
+    fclose(fd);
 
 }
 
@@ -56,6 +102,46 @@ void concurrentWrite(void *args){
 
     pthread_t id = pthread_self();
     printf("\n[w] Thread N. %ld\n", id);
+
+    char buffer[256];
+    FILE *fd;
+
+
+    strcpy(buffer, "PIPPO\0");
+    int len = strlen(buffer);
+
+    printf("\nLen = %d\n", len);
+
+    msg_t my_msg = {
+        .author = getppid(),
+        .size = sizeof(char),
+        .count = len,
+        .buffer = &buffer
+    };
+
+
+    fd = fopen((char*)args, "w"); 
+
+    if(fd == NULL){
+        printf("Error while opening the group file\n");
+        return -1;
+    }
+
+
+    int ret = fwrite(&my_msg, sizeof(msg_t), 1, fd);
+
+    printf("Totel element written: %ld", ret);
+    printf("\nsizeof(msg_t) = %ld", sizeof(msg_t));
+
+
+    //Random Sleep
+    long sleep_time = rand()%10000;
+    usleep(sleep_time);
+
+
+    fclose(fd);
+
+    return;
 
 }
 
@@ -191,16 +277,23 @@ int main(int argc, char *argv[]){
             else
                 err = pthread_create(&(tid[i]), NULL, &concurrentWrite, (void*)argv[2]);
 
-
-
             if(err < 0){
                 printf("\n[%d] Errror while creating the thread", i);
             }
-       
         }
 
 
+        for(i=0; i<THREAD_NUM; i++){
+            long t_sleep = rand()%10000;
+            t_nanosleep(t_sleep);
 
+            err = pthread_join(tid[i], NULL);
+            if(err < 0){
+                printf("\n[%d] Errror while joining the thread", i);
+            }
+        }
+        
+        printf("\n\nTerminated\n\n");
 
     }else{
         printf("Unimplemented\n\n");
